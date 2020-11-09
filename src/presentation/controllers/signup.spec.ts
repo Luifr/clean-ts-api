5a7938eda32b10bbeb6b8e682284e47adf834dac
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-11-09 19:58:59 -0300

    test: signtup controller returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,7 +2,7 @@ import { SignUpController } from './signup';
 
 describe('SignUp Controller', () => {
 	test('Should return 400 if no name is provided', () => {
-
+		// sut === System under test
 		const sut = new SignUpController();
 		const httpRequest = {
 			body: {
@@ -16,5 +16,22 @@ describe('SignUp Controller', () => {
 		const httpResponse = sut.handle(httpRequest);
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+	});
+
+	test('Should return 400 if no email is provided', () => {
+
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				name: 'anyName',
+				// email: 'any@any.com',
+				password: 'anyPassword',
+				passwordConfirmation: 'anyPassword'
+			}
+		};
+
+		const httpResponse = sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.body).toEqual(new Error('Missing param: email'));
 	});
 });
