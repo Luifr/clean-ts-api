0b2f5b7ffbdae4d9ec35393bc7514b2a9b3ce15e
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-11-13 12:39:00 -0300

    test: signpm controller test for each missing property

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -35,4 +35,38 @@ describe('SignUp Controller', () => {
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new MissingParamError('email'));
 	});
+
+	test('Should return 400 if no password is provided', () => {
+
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				name: 'anyName',
+				email: 'any@any.com',
+				// password: 'anyPassword',
+				passwordConfirmation: 'anyPassword'
+			}
+		};
+
+		const httpResponse = sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.body).toEqual(new MissingParamError('password'));
+	});
+
+	test('Should return 400 if no passwordConfirmation is provided', () => {
+
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				name: 'anyName',
+				email: 'any@any.com',
+				password: 'anyPassword',
+				// passwordConfirmation: 'anyPassword'
+			}
+		};
+
+		const httpResponse = sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'));
+	});
 });
