889243ef2be780fab4266565200e867a7b62a44d
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-11-13 12:05:46 -0300

    refactor: simplify signup controller error handle

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,14 @@ import { IHttpRequest, IHttpResponse } from '../protocols/http';
 
 export class SignUpController {
 	handle(httpRequest: IHttpRequest): IHttpResponse {
-		if (!httpRequest.body.name) {
-			return badRequest(new MissingParamError('name'));
+		const requiredFields = ['name', 'email'];
+
+		for (const requiredField of requiredFields) {
+			if (!httpRequest.body[requiredField]) {
+				return badRequest(new MissingParamError(requiredField));
+			}
 		}
-		else if (!httpRequest.body.email) {
-			return badRequest(new MissingParamError('email'));
-		}
+
 		return {
 			statusCode: 200,
 			body: {}
