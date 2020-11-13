b13e1858070bb3ffada960f26d7a0e77ce0398bd
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-11-13 12:28:46 -0300

    feat: generic controller interface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,11 @@
 import { MissingParamError } from '../errors/missing-param-name';
 import { badRequest } from '../helpers/http-helper';
+import { IController } from '../protocols/controller';
 import { IHttpRequest, IHttpResponse } from '../protocols/http';
 
-export class SignUpController {
+export class SignUpController implements IController{
 	handle(httpRequest: IHttpRequest): IHttpResponse {
-		const requiredFields = ['name', 'email'];
+		const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 
 		for (const requiredField of requiredFields) {
 			if (!httpRequest.body[requiredField]) {
