a785f969cb1fe0541ca7b94578a4dbba4b2671a0
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-11-13 12:03:05 -0300

    feat: http missing param error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,17 +1,18 @@
+import { MissingParamError } from '../errors/missing-param-name';
+import { badRequest } from '../helpers/http-helper';
+import { IHttpRequest, IHttpResponse } from '../protocols/http';
 
 export class SignUpController {
-	handle(httpRequest: any): any {
+	handle(httpRequest: IHttpRequest): IHttpResponse {
 		if (!httpRequest.body.name) {
-			return {
-				statusCode: 400,
-				body: new Error('Missing param: name')
-			};
+			return badRequest(new MissingParamError('name'));
 		}
 		else if (!httpRequest.body.email) {
-			return {
-				statusCode: 400,
-				body: new Error('Missing param: email')
-			};
+			return badRequest(new MissingParamError('email'));
 		}
+		return {
+			statusCode: 200,
+			body: {}
+		};
 	}
 }
